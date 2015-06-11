4ca43f9c13d09154c19878aff07887cbd01c3141
Caro <carolin.stellmacher@haw-hamburg.de>
2015-06-11 17:10:59 +0200

    first little low poly ground

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 function init(){
     var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0x5B92D8));
+    renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMapEnabled = true;
 
     var scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
-    camera.position.set(-40, 15, 15);
+    camera.position.set(-60, 30, 15);
     camera.lookAt(scene.position);
 
     var spotLight = new THREE.SpotLight(0xffffff);
@@ -15,28 +15,10 @@ function init(){
     spotLight.position.set(-20, 35, 40);
     scene.add(spotLight);
 
-    var axes = new THREE.AxisHelper(30);
-    scene.add(axes);
-
-    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    var cubeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xff000
-    });
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.x = 2;//eig mit Vektoren!! ändern!
-    cube.position.y = 2;
-    cube.position.z = 0;
-    cube.castShadow = true;
-    scene.add(cube);
-
-    var planeGeometry = new THREE.PlaneGeometry(30, 30);
-    var planeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xffffff
-    });
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -0.5*Math.PI;
-    plane.receiveShadow = true;
+    var plane = doGround(doGroundGeometry(50, 50, 20, 20));
     scene.add(plane);
+
+
 
     document.getElementById("output").appendChild(renderer.domElement);
     callback = function(){
@@ -44,6 +26,31 @@ function init(){
     };
     requestAnimationFrame(render);
 };
+
+var doGroundGeometry = function(width, height, widthSegments, heightSegments) {
+    var groundGeometry = new THREE.PlaneGeometry(width, height, 10, 20);
+
+    for (var i = 0; i < groundGeometry.vertices.length; i++) {
+        groundGeometry.vertices[i].x += Math.random() * 5;
+        groundGeometry.vertices[i].y += Math.random() * 2;
+        groundGeometry.vertices[i].z += Math.random() * 2;
+    }
+
+    groundGeometry.dynamic = true;
+    groundGeometry.computeFaceNormals();
+    groundGeometry.computeVertexNormals();
+    groundGeometry.normalsNeedUpdate = true;
+    return groundGeometry;
+};
+
+var doGround = function(groundGeometry) {
+    var groundMaterial = new THREE.MeshLambertMaterial({color: 0x91D94A, shading: THREE.FlatShading});
+    var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    ground.rotation.x = -0.5*Math.PI;
+    //ground.receiveShadow = true;
+    return ground;
+};
+
 
 function render(){
     callback();
