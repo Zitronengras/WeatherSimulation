7e40abd4597b7818e5a09357cf477460823225b0
Caro <carolin.stellmacher@haw-hamburg.de>
2015-06-20 20:04:37 +0200

    optimized ground and red cloud

--- code.js
+++ code.js
@@ -13,9 +13,9 @@ function init(){
     var scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 1000);
-    camera.position.x = 200;
-    camera.position.y = 200;
-    camera.position.z = 200;
+    camera.position.x = 300;
+    camera.position.y = 300;
+    camera.position.z = 300;
     //camera.lookAt(scene.position);
 
     //AxesHelper
@@ -25,8 +25,10 @@ function init(){
     //light
     var spotLight = new THREE.SpotLight(0xffffff);
     spotLight.castShadow = true;
-    spotLight.position.set(-20, 100, 600);
-    spotLight.intensity = 3;
+    spotLight.position.x = 900; //red axis
+    spotLight.position.y = 700; //green axis
+    spotLight.position.z = 1;
+    spotLight.intensity = 1.2;
     scene.add(spotLight);
 
     /*var ambientLight = new THREE.AmbientLight(0x222222);
@@ -34,8 +36,12 @@ function init(){
 
 
     //defaultGround
-    var defaultGround = doGround(doGroundGeometry(500, 500, 300, 300));
+    var defaultGround = doGround(doGroundGeometry(500, 500));
     scene.add(defaultGround);
+
+    //shows vertexNormals
+    //var edges = new THREE.VertexNormalsHelper( defaultGround, 20, 0x00ff00, 1 );
+    //scene.add(edges);
 
     //manager
     var manager = new THREE.LoadingManager();
@@ -43,17 +49,17 @@ function init(){
         console.log( item, loaded, total );
     };
     //loader
-
-
-    var dae;
+    var cloud;
     var loader = new THREE.ColladaLoader();
     loader.options.convertUpAxis = true;
     loader.load('dae/wolken.dae', function(collada){
-        dae = collada.scene;
-        console.log('obj loaded');
-        dae.scale.x = dae.scale.y = dae.scale.z = 5;
-        dae.updateMatrix();
-        scene.add(dae);
+        cloud = collada.scene;
+        //console.log('obj loaded');
+        cloud.castShadow = true;
+        cloud.scale.x = cloud.scale.y = cloud.scale.z = 1;
+        cloud.position.set(1, 80, 1);
+        cloud.updateMatrix();
+        scene.add(cloud);
     });
 
     var elem = document.getElementById("output");
@@ -79,14 +85,12 @@ function init(){
 };
 
 var doGroundGeometry = function(width, height, widthSegments, heightSegments) {
-    var groundGeometry = new THREE.PlaneGeometry(width, height, 100, 60);
-
+    var groundGeometry = new THREE.PlaneGeometry(width, height, 60, 50);
     for (var i = 0; i < groundGeometry.vertices.length; i++) {
-        groundGeometry.vertices[i].x += Math.random() * 5;
-        groundGeometry.vertices[i].y += Math.random() * 2;
-        groundGeometry.vertices[i].z += Math.random() * 2;
+        groundGeometry.vertices[i].x += Math.random() * 3; //red axis
+        groundGeometry.vertices[i].y += Math.random() * 2; //blue axis
+        groundGeometry.vertices[i].z += Math.random() * 8; //green axis: height
     }
-
     groundGeometry.dynamic = true;
     groundGeometry.computeFaceNormals();
     groundGeometry.computeVertexNormals();
@@ -98,7 +102,7 @@ var doGround = function(groundGeometry) {
     var groundMaterial = new THREE.MeshLambertMaterial({color: 0x91D94A, shading: THREE.FlatShading});
     var ground = new THREE.Mesh(groundGeometry, groundMaterial);
     ground.rotation.x = -0.5*Math.PI;
-    //ground.receiveShadow = true;
+    ground.receiveShadow = true;
     return ground;
 };
 
