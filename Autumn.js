067c3d3ae06002c7929a3e4321af5b697f09946b
Karoline Bergmann <karos1369@gmail.com>
2015-07-08 22:03:00 +0200

    4 different twisted trees for autumn

--- Autumn.js
+++ Autumn.js
@@ -34,18 +34,62 @@ function Autumn(yOffset) {
         scene.add(autumnGround);
         
         
-        //twisted tree loader
+        //Twisted red-orange
         twistedTreeLoader = new THREE.ColladaLoader();
         twistedTreeLoader.options.convertUpAxis = true;
         twistedTreeLoader.load('dae/autumn/trees/twisted_autumn1.dae', function(collada){
             twistedTree = collada.scene;
             console.log('twistedTree loaded');
             twistedTree.castShadow = true;
-            twistedTree.scale.x = twistedTree.scale.y = twistedTree.scale.z = 0.05;
+            twistedTree.scale.x = twistedTree.scale.y = twistedTree.scale.z = 1;
             shadow.addShadow(twistedTree);
-            twistedTree.position.set(10, 10+yOffset, 80);
+            twistedTree.position.set(10, 100+yOffset, 80);
             twistedTree.updateMatrix();
             scene.add(twistedTree);
+        });
+        
+         //Twisted orange-red
+        twistedTreeLoader2 = new THREE.ColladaLoader();
+        twistedTreeLoader2.options.convertUpAxis = true;
+        twistedTreeLoader2.load('dae/autumn/trees/twisted_autumn2.dae', function(collada){
+            twistedTree2 = collada.scene;
+            console.log('twistedTree loaded');
+            twistedTree2.castShadow = true;
+            twistedTree2.scale.x = twistedTree2.scale.y = twistedTree2.scale.z = 1;
+            shadow.addShadow(twistedTree2);
+            twistedTree2.position.set(30, 100+yOffset, 80);
+            twistedTree2.updateMatrix();
+            scene.add(twistedTree2);
+        });
+
+ 	//Twisted Orange
+        twistedTreeLoader3 = new THREE.ColladaLoader();
+        twistedTreeLoader3.options.convertUpAxis = true;
+        twistedTreeLoader3.load('dae/autumn/trees/twisted_autumn3.dae', function(collada){
+            twistedTree3 = collada.scene;
+            console.log('twistedTree loaded');
+            shadow.addShadow(twistedTree3);
+            twistedTree3.scale.x = twistedTree3.scale.y = twistedTree3.scale.z = 1;
+
+			twistedTree3.position.set(40, 100+yOffset, 70);
+            twistedTree3.updateMatrix();
+            scene.add(twistedTree3);
+          	shadow.addShadow(twistedTree3);
+        });
+        
+         //Twisted Red 
+        twistedTreeLoader4 = new THREE.ColladaLoader();
+        twistedTreeLoader4.options.convertUpAxis = true;
+        twistedTreeLoader4.load('dae/autumn/trees/twisted_autumn4.dae', function(collada){
+            twistedTree4 = collada.scene;
+            console.log('twistedTree loaded');
+            shadow.addShadow(twistedTree4);
+            twistedTree4.scale.x = twistedTree4.scale.y = twistedTree4.scale.z = 1;
+
+			twistedTree4.position.set(50, 100+yOffset, 70);
+            twistedTree4.updateMatrix();
+            scene.add(twistedTree4);
+          	shadow.addShadow(twistedTree4);
         });
 
         //audio
@@ -56,6 +100,9 @@ function Autumn(yOffset) {
         scene.remove(autumnGround);
         scene.remove(autumnSpotLight);
         scene.remove(twistedTree);
+        scene.remove(twistedTree2);
+         scene.remove(twistedTree3);
+          scene.remove(twistedTree4);
         scene.remove(autumnSkybox);
         autumnSkybox.remove(scene);
         scene.remove(shadow);
