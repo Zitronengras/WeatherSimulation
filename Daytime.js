abdc4c4f2893de2cca8f524fc459af7fd94f6a14
Caro <carolin.stellmacher@haw-hamburg.de>
2015-07-08 23:17:29 +0200

    add query for sunGoesDown

--- Daytime.js
+++ Daytime.js
@@ -4,26 +4,35 @@
 
 function Daytime(scene){
 
-
-    var direction = new THREE.Vector3(0,1,0);//ändern
-
-
-    var sphereGeometry = new THREE.SphereGeometry(50);
+    var direction;
+    var sunHigh = 900;
+    var sunGoesDown = -1;
+    var sphereGeometry = new THREE.SphereGeometry(20);
     var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
     var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.x = 0;
     sphere.position.y = 0;
     sphere.position.z = 0;
     scene.add(sphere);
+    console.log('DAYTIME');
 
     this.moveSun = function(mainSpotlight, optSpotlight){
 
-        mainSpotlight.velocity = direction;
-        optSpotlight.velocity = direction;
+        //mainSpotlight.velocity = direction;
+        //optSpotlight.velocity = direction;
+        if(sunHigh == 0){
+            direction = new THREE.Vector3(0,1,0);//ändern
+            sunGoesDown = +1;
+        }
+        if(sunHigh == 900){
+            direction = new THREE.Vector3(0,-1,0);//ändern
+            sunGoesDown = -1;
+        }
+        //if(sunGoesDown > 0){
 
-        mainSpotlight.position.add(mainSpotlight.velocity);
-        optSpotlight.position.add(optSpotlight.velocity);
-
-
+            mainSpotlight.position.add(direction);
+            optSpotlight.position.add(direction);
+            console.log('move');
+        //}
     }
 }
