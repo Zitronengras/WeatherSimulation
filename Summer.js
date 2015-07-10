380780aed077b94e5c2801a3353ba47a7d995222
Caro <carolin.stellmacher@haw-hamburg.de>
2015-07-10 14:53:16 +0200

    remove cloud from summer.js

--- Summer.js
+++ Summer.js
@@ -48,19 +48,6 @@ function Summer(yOffset) {
         summerGround = ground.doGround(ground.doGroundGeometry(), summerGroundColor);
         scene.add(summerGround);
 
-        //cloud loader
-        cloudLoader = new THREE.ColladaLoader();
-        cloudLoader.options.convertUpAxis = true;
-        cloudLoader.load('dae/Wolken.dae', function(collada){
-            cloud = collada.scene;
-            console.log('cloud loaded');
-            cloud.scale.x = cloud.scale.y = cloud.scale.z = 0.5;
-            cloud.position.set(1, 100, 1);
-            shadow.addShadow(cloud);
-            cloud.updateMatrix();
-            scene.add(cloud);
-        });
-
         //grass loader
         grassStalkLoader = new THREE.ColladaLoader();
         grassStalkLoader.options.convertUpAxis = true;
