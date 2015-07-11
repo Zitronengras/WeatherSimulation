b72810fa3e0b337ddabbfd5fe02941220b9c9c66
Magnifica <magnifica92@hotmail.de>
2015-07-11 15:40:22 +0200

    water polygons

--- Water.js
+++ Water.js
@@ -2,24 +2,23 @@ function Water(yOffset) {
 
     this.doWaterGeometry = function() {
         var waterGeometry = new THREE.PlaneGeometry(500, 500, 40, 80);
-        var heightI = 1;
+       
         for (var i = 0; i < waterGeometry.vertices.length; i++) {
             if(i > 13000 && i < 20900){
                 waterGeometry.vertices[i].x += Math.random() * 3; //red axis
                 waterGeometry.vertices[i].y += Math.random() * 2; //blue axis
-                waterGeometry.vertices[i].z += Math.random() * 26; //green axis: height 11
+                waterGeometry.vertices[i].z += Math.random() * 20; //green axis
             }
             else if(i > 20900){
                 waterGeometry.vertices[i].x += Math.random() * 3; //red axis
                 waterGeometry.vertices[i].y += Math.random() * 6; //blue axis
-                waterGeometry.vertices[i].z += Math.random() * 28; //green axis: height 9
+                waterGeometry.vertices[i].z += Math.random() * 28; //green axis
             }
             else{
                 waterGeometry.vertices[i].x += Math.random() * 3; //red axis
                 waterGeometry.vertices[i].y += Math.random() * 2; //blue axis
-                waterGeometry.vertices[i].z += Math.random() * 25; //green axis: height 8
+                waterGeometry.vertices[i].z += Math.random() * 15; //green axis
             }
-        
         }
         waterGeometry.dynamic = true;
         waterGeometry.computeFaceNormals();
@@ -27,6 +26,7 @@ function Water(yOffset) {
         waterGeometry.normalsNeedUpdate = true;
         return waterGeometry;
     };
+   
     this.doWater = function(waterGeometry) {
         var waterMaterial = new THREE.MeshLambertMaterial(
             {color: 0x6EFAFF, shading: THREE.FlatShading, transparent: true, opacity: 0.9}); 
