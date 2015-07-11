98ff3d801a1c6adc27a1df3bc8a7745999d76f14
Magnifica <magnifica92@hotmail.de>
2015-07-11 04:51:39 +0200

    River

--- River.js
+++ River.js
@@ -4,133 +4,133 @@ function River () {
     
             
     // geometry
-var geometry = new THREE.Geometry();
-geometry.vertices.push( new THREE.Vector3(-250, 0, -150 ) );
-geometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-250, 0, -150 ) ); // close the loop 1
+var riverGeometry = new THREE.Geometry();
+riverGeometry.vertices.push( new THREE.Vector3(-250, 0, -150 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-250, 0, -150 ) ); // close the loop 1
         
-geometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -100) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) ); // close the loop 2
+riverGeometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -100) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-250, 0, -100 ) ); // close the loop 2
         
-geometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) ); // close the loop 3
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) ); // close the loop 3
 
-geometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) ); // close the loop 4
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-200, 0, -50 ) ); // close the loop 4
 
-geometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-100, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) ); // close the loop 5
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) ); // close the loop 5
         
-geometry.vertices.push( new THREE.Vector3(-100, 0, -50) );
-geometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
-geometry.vertices.push( new THREE.Vector3(-100, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3(-100, 0, -50 ) ); // close the loop 6
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, -50) );
+riverGeometry.vertices.push( new THREE.Vector3(-150, 0, -50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, -50 ) ); // close the loop 6
         
-geometry.vertices.push( new THREE.Vector3(-100, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3(-100, 0, -50) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3(-100, 0, 0  ) ); // close the loop 7
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, -50) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-100, 0, 0  ) ); // close the loop 7
         
-geometry.vertices.push( new THREE.Vector3( -100, 0, 50) );
-geometry.vertices.push( new THREE.Vector3( -100, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) ); // close the loop 8
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 50) );
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) ); // close the loop 8
 
-geometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
-geometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) ); // close the loop 9
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 0 ) ); // close the loop 9
 
-geometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 100 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
-geometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) ); // close the loop 10
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 100 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -100, 0, 50 ) ); // close the loop 10
         
-geometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
-geometry.vertices.push( new THREE.Vector3(-50, 0, 100 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) ); // close the loop 11
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-50, 0, 100 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 50 ) ); // close the loop 11
         
-geometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
-geometry.vertices.push( new THREE.Vector3(-50, 0, 100) );
-geometry.vertices.push( new THREE.Vector3(-50, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) ); // close the loop 12
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
+riverGeometry.vertices.push( new THREE.Vector3(-50, 0, 100) );
+riverGeometry.vertices.push( new THREE.Vector3(-50, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) ); // close the loop 12
         
-geometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) ); // close the loop 13
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 100 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) ); // close the loop 13
 
-geometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3(0, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3( -50, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) ); // close the loop 14
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3(0, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3( -50, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) ); // close the loop 14
 
-geometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 50, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) );
-geometry.vertices.push( new THREE.Vector3(  0, 0, 200  ) ); // close the loop 15
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 150 ) );
+riverGeometry.vertices.push( new THREE.Vector3(  0, 0, 200  ) ); // close the loop 15
         
-geometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 50, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) ); // close the loop 16
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) ); // close the loop 16
         
-geometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) ); // close the loop 17
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 0, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) ); // close the loop 17
         
-geometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) ); // close the loop 18
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 50, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) ); // close the loop 18
 
-geometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3(100, 0, 250 ) ); // close the loop 19
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3(100, 0, 250 ) ); // close the loop 19
 
-geometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) ); // close the loop 20
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 100, 0, 200 ) ); // close the loop 20
        
-geometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) );
-geometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) ); // close the loop 21
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 200 ) ); // close the loop 21
         
-geometry.vertices.push( new THREE.Vector3(150, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) );
-geometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) ); // close the loop 22
+riverGeometry.vertices.push( new THREE.Vector3(150, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 150, 0, 250 ) ); // close the loop 22
         
-geometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) );
-geometry.vertices.push( new THREE.Vector3( 250, 0, 300 ) );
-geometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) ); // close the loop 23
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 250, 0, 300 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 300 ) ); // close the loop 23
 
-geometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 250, 0, 250 ) );
-geometry.vertices.push( new THREE.Vector3( 250, 0, 300 ) );
-geometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) ); // close the loop 24
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 250, 0, 250 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 250, 0, 300 ) );
+riverGeometry.vertices.push( new THREE.Vector3( 200, 0, 250 ) ); // close the loop 24
      
        
 // material
-var material = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 1 } );
+var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
 
 // line
-var line = new THREE.Line( geometry, material );
+var line = new THREE.Line( riverGeometry, material );
 scene.add( line ); 
         
   console.log('river loaded');
