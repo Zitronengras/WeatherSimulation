4b07f07118c38de86815030e9e26f254b440cd60
Magnifica <magnifica92@hotmail.de>
2015-07-14 13:09:17 +0200

    river position

--- River.js
+++ River.js
@@ -11,31 +11,15 @@ function River(yOffset) {
             }
             else if(i > 1000){
                 riverGeometry.vertices[i].x += Math.random() * 3; //red axis
-                riverGeometry.vertices[i].y += Math.random() * 6; //blue axis
-                riverGeometry.vertices[i].z += Math.random() * 28; //green axis
+                riverGeometry.vertices[i].y += Math.random() * 2; //blue axis
+                riverGeometry.vertices[i].z += Math.random() * 15; //green axis
             }
             else{
                 riverGeometry.vertices[i].x += Math.random() * 3; //red axis
                 riverGeometry.vertices[i].y += Math.random() * 2; //blue axis
-                riverGeometry.vertices[i].z += Math.random() * 15; //green axis
+                riverGeometry.vertices[i].z += Math.random() * 20; //green axis
             }
-		    //height
-            if(i > 18000 && i <= 21000){
-                groundGeometry.vertices[i].z += heightI; //green axis: height
-                heightI += 0.0015;
-            }
-            else if(i > 21000 && i <= 25000){
-                groundGeometry.vertices[i].z += heightI; //green axis: height
-                heightI += 0.0036;
-            }
-            else if(i > 25000 && i <= 26000) {
-                groundGeometry.vertices[i].z += heightI; //green axis: height
-                heightI += 0.0045;
-            }
-            else if(i > 26000){
-                groundGeometry.vertices[i].z += heightI; //green axis: height
-                heightI += 0.0015;
-            }
+		  
         }
         riverGeometry.dynamic = true;
         riverGeometry.computeFaceNormals();
@@ -48,16 +32,16 @@ function River(yOffset) {
         var riverMaterial = new THREE.MeshLambertMaterial(
             {color: 0x6EFAFF, shading: THREE.FlatShading}); 
         river = new THREE.Mesh(riverGeometry, riverMaterial);
-        river.rotation.x = -0.5*Math.PI;
+        river.rotation.x = -0.6*Math.PI;
         river.position.z = 150;
-        river.position.y = yOffset;
+        river.position.y = 5+yOffset;
 		//river.rotation.z = 0.3;
         river.receiveShadow = true;
         return river;
 		
 		
     };
-    var max = 20;
+    var max = 10;
     var min = 1;
     
     this.animateRiver = function (sea){
@@ -67,7 +51,7 @@ function River(yOffset) {
             var diff = Math.random() * (1 -(- 1)) + (-1);
             while(Math.abs(diff-before)>0.2){
                 diff = Math.random() * (1 -(- 1)) + (-1)}
-            before =diff;
+				before =diff;
             if(vertex.z > max){
             vertex.add(new THREE.Vector3 (0,0, Math.abs(diff)*(-1)));
             } 
