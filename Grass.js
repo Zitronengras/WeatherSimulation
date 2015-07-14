84f32e8a150667daed4fc1b397ed6ba6c43b95f7
Caro <carolin.stellmacher@haw-hamburg.de>
2015-07-14 15:43:47 +0200

    add setGrassPosition() in grass.js

--- Grass.js
+++ Grass.js
@@ -18,7 +18,7 @@ function Grass(){
         grassStalkLoader = new THREE.ColladaLoader();
         grassStalkLoader.options.convertUpAxis = true;
         grassArray = [];
-        grassStalkLoader.load('dae/Grashalm.dae', function(collada){
+        grassStalkLoader.load('dae/grass.dae', function(collada){
 
             var grassMaterial = new THREE.MeshLambertMaterial({color: color});
             grassStalk = collada.scene;
@@ -30,7 +30,7 @@ function Grass(){
                 for (var j = 0; j < colladaObj.children.length; j++) {
                     newGrassStalk.add(new THREE.Mesh(colladaObj.children[j].geometry, grassMaterial));
                 }
-                newGrassStalk.scale.x = newGrassStalk.scale.y = newGrassStalk.scale.z = 0.09;
+                newGrassStalk.scale.x = newGrassStalk.scale.y = newGrassStalk.scale.z = 5;
                 newGrassStalk.position.set(0,0,0);
 
                 newGrassStalk.rotation.x = -90*Math.PI/180;
@@ -61,7 +61,7 @@ function Grass(){
             grassStalk.scale.x = grassStalk.scale.y = grassStalk.scale.z = 0.05;
             //grassStalk.position.set(-3,yOffset,-3);
             shadow.addShadow(grassStalk);
-            setPosition(grassStalk, ground, getVerticesArray(), 0, 0);
+            setGrassPosition(grassStalk, ground, getVerticesArray(), 0, -100);
             grassStalk.updateMatrix();
             console.log('grassStalk loaded');
 
@@ -83,4 +83,116 @@ function Grass(){
     function getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
     }
+}
+
+
+function setGrassPosition(object, ground, array, xPos, zPos){
+    console.log('vor zurodung position');
+    console.log(object.position);
+
+    if(xPos != null){
+        object.position.x = xPos;
+        console.log('zuordnung position x');
+        console.log(object.x);
+    }
+    if(zPos != null){
+        object.position.z = zPos;
+
+        console.log('zuordnung position z');
+        console.log(object.z);
+    }
+    console.log('nach zurodung position');
+    console.log(object.position);
+
+    var range = 4;
+    var minX = xPos - range;
+    var maxX = xPos + range;
+    var minZ = zPos - range;
+    var maxZ = zPos + range;
+
+    var xCo;
+    var yCo;
+    var zCo;
+
+    for(var i=0; i < array.length; i++) {
+        if(Math.round(array[i].x) > minX && Math.round(array[i].x) < maxX
+            && Math.round(array[i].y) > minZ && Math.round(array[i].y) < maxZ){
+            yGround = Math.round(array[i].y);
+            xGround = Math.round(array[i].x);
+            zGround = Math.round(array[i].z);
+        }
+    }
+
+    ground.updateMatrixWorld();
+    //yGround = yGround/2;
+    var worldCo = new THREE.Vector3(xGround, yGround, zGround);
+    ground.localToWorld(worldCo);
+    console.log('worldCo');
+    console.log(worldCo);
+
+    //move height/2 on y axis
+    var box = new THREE.Box3().setFromObject(object); //creates boundingbox
+    var boxMin = box.min.y;
+    var boxMax = box.max.y;
+
+    //console.log(box.min, box.max, box.size() );
+    console.log('box size');
+    console.log(box.size());
+    console.log('box.min' + box.min.y);
+    console.log('box.min');
+    console.log(box.min.y);
+    console.log('box.max' + box.max.y);
+    console.log('worldCo.y');
+    console.log(worldCo.y);
+
+
+    //console.log(' min ' + boxMin + ' max ' + boxMax);*/
+
+    //calculate height/2
+    var offset = boxMax - boxMin;
+    console.log('max - min' + offset);
+    if(offset < 0){
+        offset = offset * (-1);
+        console.log('mal - 1' + offset);
+    }
+    offset = offset/2;
+    console.log('offset / 2', offset);
+
+    offset = worldCo.y + offset;
+    console.log('wolrd.y + offset');
+    console.log(offset);
+
+    offset -= 10;
+
+
+    worldCo.setY(offset);
+
+    console.log('worldCo.SetY');
+    console.log(worldCo.y);
+    /*
+     console.log('worldCo.y');
+     console.log(worldCo.y);
+     worldCo.setY((worldCo.y + offset));
+     console.log('worldCo');
+     console.log(worldCo);
+
+     object.updateMatrix();
+
+     object.position.copy(worldCo);
+     console.log('object position');
+     console.log(object.position);*/
+
+    //worldCo.setY();
+    object.updateMatrix();
+
+
+    object.position.copy(worldCo);
+
+    object.updateMatrix();
+
+    console.log('object position');
+    console.log(object.position);
+
+
+    //return object.position.copy(worldCo);
 }
